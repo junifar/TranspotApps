6841a3960e3c0c29d6e72c6e5e28bb80133b3bd4
junifar <junifar@gmail.com>
2016-08-04 15:49:53 +0700

    Initial commit

---
+++ app/src/main/java/com/transpot/rubahapi/transpot/LoginActivity.java
@@ -0,0 +1,112 @@
+package com.transpot.rubahapi.transpot;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView mUsernameView;
+    private EditText mPasswordView;
+    private View mLoginFormView;
+    private View mProgressView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUsernameView = (AutoCompleteTextView) findViewById(R.id.username);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        Button signInButton = (Button) findViewById(R.id.email_sign_in_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateLoginForm();
+            }
+        });
+
+        mLoginFormView = findViewById(R.id.login_form);
+        mProgressView = findViewById(R.id.login_progress);
+    }
+
+    private void validateLoginForm() {
+
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
+        String username = mUsernameView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(password)){
+            mPasswordView.setError("Field Required");
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(username)){
+            mUsernameView.setError("Field Required");
+            focusView = mUsernameView;
+            cancel = true;
+        }
+
+        if (cancel){
+            focusView.requestFocus();
+        } else {
+            showProgress(true);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2){
+
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mLoginFormView.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(
+                    new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animator) {}
+
+                        @Override
+                        public void onAnimationEnd(Animator animator) {
+                            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animator) {}
+
+                        @Override
+                        public void onAnimationRepeat(Animator animator) {}
+                    });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(
+                    new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                        }
+                    });
+        }else {
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
+}
---
+++ app/src/main/java/com/transpot/rubahapi/transpot/controller/net/TranspotService.java
@@ -0,0 +1,51 @@
+package com.transpot.rubahapi.transpot.controller.net;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Url;
+
+/**
+ * Created by junifar on 02-Aug-16.
+ */
+public interface TranspotService {
+
+    @Headers("User Agent : RubahApi")
+    @GET("http://localhost:8000/members")
+    Call<LoginValues> getLoginValues();
+
+    @Headers("User Agent : RubahApi")
+    @POST
+    Call<LoginResponse> login(@Url String url);
+
+    @Headers("User Agent : RubahApi")
+    @POST
+    Call<ResponseBody> requestToken(@Url String url);
+
+    class LoginValues {
+
+        private String lt;
+        private String execution;
+
+        public LoginValues(){}
+
+        public String getExecution() {
+            return execution;
+        }
+
+        public String getLt() {
+            return lt;
+        }
+    }
+
+    class LoginResponse{
+        public LoginResponse(){}
+    }
+
+    class LoginError{
+        private String[] errors;
+    }
+
+}
